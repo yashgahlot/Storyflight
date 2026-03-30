a97cfcc93d8051b3296ba8fddaf88953c52082a1
yash gahlot <ygahlot077@gmail.com>
2026-03-30 04:01:20 -0300

    Initial portfolio story update

---
+++ src/components/TextSection.jsx
@@ -0,0 +1,42 @@
+import { Text } from "@react-three/drei";
+import { fadeOnBeforeCompileFlat } from "../utils/fadeMaterial";
+
+export const TextSection = ({ title, subtitle, ...props }) => {
+  return (
+    <group {...props}>
+      {!!title && (
+        <Text
+          color="white"
+          anchorX={"left"}
+          anchorY="bottom"
+          fontSize={0.42}
+          maxWidth={3.2}
+          lineHeight={1}
+          font={"./fonts/DMSerifDisplay-Regular.ttf"}
+        >
+          {title}
+          <meshStandardMaterial
+            color={"white"}
+            onBeforeCompile={fadeOnBeforeCompileFlat}
+          />
+        </Text>
+      )}
+
+      <Text
+        color="white"
+        anchorX={"left"}
+        anchorY="top"
+        fontSize={0.17}
+        maxWidth={3.2}
+        lineHeight={1.35}
+        font={"./fonts/Inter-Regular.ttf"}
+      >
+        {subtitle}
+        <meshStandardMaterial
+          color={"white"}
+          onBeforeCompile={fadeOnBeforeCompileFlat}
+        />
+      </Text>
+    </group>
+  );
+};
